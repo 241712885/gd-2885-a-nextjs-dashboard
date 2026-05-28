01ae86a344b3707c522fd4f8707047640d762ee5
241712885 <mirandaimanuekka@gnail.com>
2026-05-28 11:29:50 +0700

    kelas diks

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -66,3 +66,13 @@ export type CustomerForm = {
   email: string;
   image_url: string;
 };
+
+export type FormattedCustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: string;
+  total_paid: string;
+};
